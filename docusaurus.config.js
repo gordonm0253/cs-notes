ec3829b2ec8a98e42ae908cf7ab0dec184c9f952
gordonm0253 <gordonm0253@gmail.com>
2026-05-15 05:52:26 -0400

    wrong link

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -20,7 +20,7 @@ const config = {
   },
 
   // Set the production url of your site here
-  url: 'https://gordonm0253.github.io/cs-notes/',
+  url: 'https://gordonm0253.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/cs-notes/',
