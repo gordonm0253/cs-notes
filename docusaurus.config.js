30022a7fde91229c4ff26b234dead83b3529f468
gordonm0253 <gordonm0253@gmail.com>
2026-05-15 05:50:42 -0400

    yarn

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -20,10 +20,10 @@ const config = {
   },
 
   // Set the production url of your site here
-  url: 'https://cs4820-algorithms-notes.example.com',
+  url: 'https://gordonm0253.github.io/cs-notes/',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/cs-notes/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
